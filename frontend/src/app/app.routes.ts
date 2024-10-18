e93314c5a9c32fb9ba7140d79567d1071e28d493
Jonel Mathew <123563661+Jonel69@users.noreply.github.com>
2024-10-18 12:33:00 +0530

    Delete frontend directory

--- frontend/src/app/app.routes.ts
+++
@@ -1,5 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    { path: 'portfolio-website', loadChildren: () => import('./portfolio-website/portfolio-website.module').then(m => m.PortfolioWebsiteModule) }
-];
